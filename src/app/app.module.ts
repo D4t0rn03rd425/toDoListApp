c8f2815c502db8d3d106aefcf4c612ad7a142975
D4t0rn03rd <81825575+D4t0rn03rd425@users.noreply.github.com>
2021-12-15 18:22:18 +0100

    added authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YellPipe } from './yell.pipe';
 import { TodoComponent } from './todo/todo.component';
+import { AuthModule, AuthHttpInterceptor } from '@auth0/auth0-angular';
 
 @NgModule({
 	declarations: [
@@ -15,9 +16,21 @@ import { TodoComponent } from './todo/todo.component';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		HttpClientModule
+		HttpClientModule,
+
+		// Import the module into the application, with configuration
+		AuthModule.forRoot({
+			domain: 'dev-3mnjwg73.us.auth0.com',
+			clientId: '6c5jJDXv4C3YmJ2J5Dsf8VUUEGeOFZAo',
+			audience: 'https://d4t0rn03rd.com/todo',
+			httpInterceptor: {
+				allowedList: ['http://localhost:3000/*']
+			}
+		}),
 	],
-	providers: [],
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
